3421314b8b741bbe90485180b3cc9ecda048ceab
Niko <niko.jartti@student.careeria.fi>
2024-11-06 19:30:20 +0000

    moveController.cs added just for testing

---
+++ Kauhupeli/Assets/moveController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Animator flashlightAnim;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
+        {
+            if(Input.GetKey(KeyCode.LeftShift))
+            {
+                flashlightAnim.ResetTrigger("walk");
+                flashlightAnim.SetTrigger("sprint");
+            }
+            else
+            {
+                flashlightAnim.ResetTrigger("sprint");
+                flashlightAnim.SetTrigger("walk");
+            }
+        }
+    }
+}
